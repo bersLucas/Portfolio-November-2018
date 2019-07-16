84d9ad0f8b38d9bdd3fa6477fe3addee5313b5c4
Lucas Bersier <contact@lucasbersier.com>
2019-07-15 22:41:17 -0400

    console.log instead of process.stdout for ubuntu

--- build.js
+++ build.js
@@ -20,36 +20,27 @@ const writeJS = files => Promise.all(
 (async () => {
   try {
     // folder scaffolding
-    process.stdout.write('\n⌛ Scaffolding folders...');
+    console.log('Scaffolding folders...');
     await Promise.all([
       fs.mkdir('dist', { recursive: true }),
       fs.mkdir('src/js/out', { recursive: true }),
     ]);
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('✔️ Scaffolding folders');
 
     // Babel
-    process.stdout.write('\n⌛ Transpiling JS...');
+    console.log('Transpiling JS...');
     let files = await fs.readdir('src/js');
     files = files.filter(file => path.extname(file).toLowerCase() === '.js');
     const transpiledFiles = await transpileJS(files);
     await writeJS(transpiledFiles);
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('✔️ Transpiling JS');
 
     // pug
-    process.stdout.write('\n⌛ Transpiling HTML...');
+    console.log('Transpiling HTML...');
     await fs.writeFile('dist/index.html', pug.renderFile('src/index.pug', {
       filename: 'src/index.pug',
     }));
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('✔️ Transpiling HTML');
 
     // sass
-    process.stdout.write('\n⌛ Transpiling CSS...');
+    console.log('Transpiling CSS...');
     const CSS = sass.renderSync({
       file: 'src/style/style.scss',
       sourceMap: true,
@@ -60,22 +51,16 @@ const writeJS = files => Promise.all(
       fs.writeFile('src/style/style.css', CSS.css),
       fs.writeFile('src/style/style.css.map', CSS.map),
     ]);
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('✔️ Transpiling CSS');
 
     // inline-source
-    process.stdout.write('\n⌛ Inlining source...');
+    console.log('Inlining source...');
     const html = await inlineSource(path.resolve('dist/index.html'), {
       compress: true,
     });
     await fs.writeFile('dist/index.html', html);
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('✔️ Inlining source');
 
-    process.stdout.write('\n✨ Done!');
+    console.log('✨ Done!');
   } catch (err) {
-    process.stdout(err);
+    console.log(err);
   }
 })();
